d00bbe64c16e19e90544b9e7cf9c15c99107e015
PrudyvusP <vgolosyandriya@yandex.ru>
2021-05-09 19:59:13 +0300

    mail_use_tls = false was throwing an exception by mail.ru-SMTP during authentication

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
-    DEBUG = True
+    DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'indoneSia XeNa'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    TEMPLATES_AUTO_RELOAD = True  # changes in templates without reloading a page
+    TEMPLATES_AUTO_RELOAD = False  # changes in templates without reloading a page
     UPLOADED_FILES_DEST = os.path.join(basedir, 'app', 'static', 'avatars')
     ALLOWED_EXTENSIONS = [".jpg", ".png", ".jpeg", ".gif"]
     MAX_CONTENT_LENGTH = 20 * 1024 * 1024  # 1mb
@@ -20,7 +20,6 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_PORT = os.environ.get('MAIL_PORT')
     MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
     MAIL_SUBJECT_PREFIX = 'FleaderT - '
 
