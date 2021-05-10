355eb2712d37e7d36756b61c3a1cc29e0f4200cc
PrudyvusP <vgolosyandriya@yandex.ru>
2021-05-10 14:48:55 +0300

    nothing special

--- fleadert.py
+++ fleadert.py
@@ -6,4 +6,4 @@ app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Task': Task, 'Project': 'Project'}
+    return {'db': db, 'User': User, 'Task': Task, 'Project': Project, 'Request': Request}
